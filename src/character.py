6ae59376111ae861acac95f75e15bb1b5ba9bc01
Marco Fracassi <marco.fracassi@motork.io>
2024-07-02 22:41:05 +0200

    iteration two

--- src/character.py
+++ src/character.py
@@ -1,17 +1,33 @@
 class Character:
 
-    def __init__(self):
-        self.level = 1
+    def __init__(self, level: int = 1):
+        self.level = level
         self.health = 1000
 
     def is_alive(self):
         return self.health > 0
 
-    def take_damage(self, amount):
-        self.health = max(0, self.health - amount)
-
-    def take_heal(self, amount):
-        if not self.is_alive():
+    def damage(self, target, amount: int):
+        if self.__is_me(target):
             return
-        self.health = min(1000, self.health + amount)
+        if self.__levels_above_mine(target) >= 5:
+            amount = amount * 0.5
+        if self.__levels_below_mine(target) >= 5:
+            amount = amount * 1.5
+        target.health = max(0, target.health - amount)
 
+    def heal(self, target, amount: int):
+        if not target.is_alive():
+            return
+        if not self.__is_me:
+            return
+        target.health = min(1000, target.health + amount)
+
+    def __is_me(self, target):
+        return target == self
+
+    def __levels_above_mine(self, target):
+        return target.level - self.level
+
+    def __levels_below_mine(self, target):
+        return self.level - target.level
--- test/characters_test.py
+++ test/characters_test.py
@@ -4,36 +4,55 @@ from src.character import Character
 
 
 class CharactersTest(unittest.TestCase):
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.character = Character()
+    def setUp(self):
+        self.character1 = Character()
+        self.character2 = Character()
 
     def test_health_starting_at_1000(self):
-        self.assertEqual(self.character.health, 1000)
+        self.assertEqual(self.character1.health, 1000)
 
     def test_level_starting_at_1(self):
-        self.assertEqual(self.character.level, 1)
+        self.assertEqual(self.character1.level, 1)
 
     def test_starting_alive(self):
-        self.assertTrue(self.character.is_alive())
+        self.assertTrue(self.character1.is_alive())
 
     def test_damage_is_subtracted_from_health(self):
-        self.character.take_damage(100)
-        self.assertEqual(self.character.health, 900)
+        self.character1.damage(self.character2, 100)
+        self.assertEqual(self.character2.health, 900)
 
     def test_when_damage_received_exceeds_current_health_it_becomes_0(self):
-        self.character.take_damage(1100)
-        self.assertEqual(self.character.health, 0)
+        self.character1.damage(self.character2, 1100)
+        self.assertEqual(self.character2.health, 0)
 
     def test_when_damage_received_exceeds_current_health_die(self):
-        self.character.take_damage(1100)
-        self.assertFalse(self.character.is_alive())
+        self.character1.damage(self.character2, 1100)
+        self.assertFalse(self.character2.is_alive())
 
     def test_dead_characters_cannot_be_healed(self):
-        self.character.take_damage(1100)
-        self.character.take_heal(800)
-        self.assertFalse(self.character.is_alive())
+        self.character1.damage(self.character2, 1100)
+        self.character2.heal(self.character2, 800)
+        self.assertFalse(self.character2.is_alive())
 
     def test_healing_cannot_raise_health_above_1000(self):
-        self.character.take_heal(200)
-        self.assertEqual(self.character.health, 1000)
+        self.character1.heal(self.character1, 200)
+        self.assertEqual(self.character2.health, 1000)
+
+    def test_character_cannot_deal_damage_to_itself(self):
+        self.character1.damage(self.character1, 200)
+        self.assertEqual(self.character1.health, 1000)
+
+    def test_character_can_heal_itself(self):
+        self.character1.damage(self.character2, 200)
+        self.character2.heal(self.character2, 100)
+        self.assertEqual(self.character2.health, 900)
+
+    def test_damage_is_reduced_by_50_percent_when_target_is_5_or_more_levels_above_the_attacker(self):
+        character3 = Character(6)
+        self.character1.damage(character3, 100)
+        self.assertEqual(character3.health, 950)
+
+    def test_damage_is_increased_by_50_percent_when_target_is_5_or_more_levels_below_the_attacker(self):
+        character3 = Character(6)
+        character3.damage(self.character1, 100)
+        self.assertEqual(self.character1.health, 850)
